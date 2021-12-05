f188b3ef13bc03201950e26a90379265abac7ef7
isubasinghe <isitha@pipeki.io>
2021-12-04 22:01:06 -0500

    Added intial files

---
+++ src/AST.hs
@@ -0,0 +1,66 @@
+module AST where
+
+import Data.Char (chr)
+import Data.Text (Text)
+
+data Op
+  = Add
+  | Sub
+  | Mult
+  | Div
+  | Power
+  | Equal
+  | Neq
+  | Less
+  | Leq
+  | Greater
+  | Geq
+  | And
+  | Or
+  | BitAnd
+  | BitOr
+  deriving (Show, Eq)
+
+data Uop
+  = Neg
+  | Not
+  deriving (Show, Eq)
+
+data Expr
+  = Literal Int
+  | StrLit Text
+  | CharLit Int
+  | FLit Double
+  | BoolLit Bool
+  | Null
+  | Id Text
+  | BinOp Op Expr Expr
+  | Unop Uop Expr
+  | Call Text [Expr]
+  | Cast Type Expr
+  | Access Expr Expr
+  | Deref Expr
+  | Addr Expr
+  | Assign Expr
+  | Sizeof Expr
+  | Noexpr
+  deriving (Show, Eq)
+
+data Statement
+  = Block [Statement]
+  | Return Expr
+  | If Expr Statement Statement
+  | For Expr Expr Expr Statement
+  | While Expr Statement
+  deriving (Show, Eq)
+
+data Type 
+  = Pointer Type 
+  | TyInt 
+  | TyBool
+  | TyChar 
+  | TyFloat 
+  | TyVoid 
+  | TyStruct Text 
+  deriving(Show, Eq)
+
