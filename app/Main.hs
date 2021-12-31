f6f63b582099e5cff85b8598d4abf6da56a320d7
isubasinghe <isubasinghe@student.unimelb.edu.au>
2021-12-31 23:09:15 +1100

    added yaml

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import qualified Options.Applicative as OP 
+import qualified MicroAST as A
+import qualified MicroParser as P 
+import qualified Data.Text.IO as TIO
+import qualified CLI as C
+import Text.Megaparsec (runParser, errorBundlePretty)
+import MicroParser (programP)
 
 main :: IO ()
-main = someFunc
+
+main = runOpts =<< OP.execParser (C.optionsP `withInfo` infoString) 
+  where 
+    withInfo opts desc = OP.info (OP.helper <*> opts) $ OP.progDesc desc 
+    infoString = "Run the mcc compiler on a given file"
+
+
+processAction :: C.Action -> A.Program -> IO ()
+processAction act ast = print ast
+
+runOpts :: C.Options -> IO ()
+runOpts (C.Options action infile) = do 
+  programSource <- TIO.readFile infile 
+  let parseTree = runParser programP infile programSource
+  case parseTree of 
+    Left err -> putStrLn $ errorBundlePretty err
+    Right ast -> processAction action ast
+
