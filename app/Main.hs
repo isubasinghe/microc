309e6a764851bdb96e25fec4df07b3622eaa0310
isubasinghe <isubasinghe@student.unimelb.edu.au>
2022-01-03 21:31:33 +1100

    added fully completed compiler

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified Options.Applicative as OP 
-import qualified MicroAST as A
-import qualified MicroParser as P 
-import qualified Data.Text.IO as TIO
-import qualified MicroSemant as MS 
 import qualified CLI as C
-import Text.Megaparsec (runParser, errorBundlePretty)
+import qualified Data.Text.IO as TIO
+import qualified MicroAST as A
 import MicroParser (programP)
+import qualified MicroParser as P
+import qualified MicroSemant as MS
+import qualified MicroTopLevel as TL
+import qualified MicroCodegen as CG
+import qualified Options.Applicative as OP
+import LLVM.Pretty (ppllvm)
+import Text.Megaparsec (errorBundlePretty, runParser)
+import Data.String.Conversions (cs)
 
 main :: IO ()
-
-main = runOpts =<< OP.execParser (C.optionsP `withInfo` infoString) 
-  where 
-    withInfo opts desc = OP.info (OP.helper <*> opts) $ OP.progDesc desc 
+main = runOpts =<< OP.execParser (C.optionsP `withInfo` infoString)
+  where
+    withInfo opts desc = OP.info (OP.helper <*> opts) $ OP.progDesc desc
     infoString = "Run the mcc compiler on a given file"
 
-
 processAction :: C.Action -> A.Program -> IO ()
-processAction act ast = case act of 
-                          C.Ast -> print ast 
-                          _ -> case MS.checkProgram ast of 
-                                 Left err -> print err 
-                                 Right sast -> print sast
+processAction act ast = case act of
+  C.Ast -> print ast
+  _ -> case MS.checkProgram ast of
+    Left err -> print err
+    Right sast ->
+      let llvm = CG.codegenProgram sast
+       in case act of
+            C.Sast -> print sast
+            C.LLVM -> TIO.putStrLn . cs . ppllvm $ llvm
+            C.Compile outfile -> undefined
+            _ -> error $ show act 
 
 runOpts :: C.Options -> IO ()
-runOpts (C.Options action infile) = do 
-  programSource <- TIO.readFile infile 
+runOpts (C.Options action infile) = do
+  programSource <- TIO.readFile infile
   let parseTree = runParser programP infile programSource
-  case parseTree of 
+  case parseTree of
     Left err -> putStrLn $ errorBundlePretty err
     Right ast -> processAction action ast
-
--- src/MicroTopLevel.hs
+++ src/MicroTopLevel.hs
@@ -13,3 +13,18 @@ import           System.Process
 import           System.Posix.Temp
 
 import           Control.Exception              ( bracket )
+
+compile :: Module -> FilePath -> IO ()
+compile llvmModule outfile =
+  bracket (mkdtemp "build") removePathForcibly $ \buildDir ->
+    withCurrentDirectory buildDir $ do
+      -- create temporary file for "output.ll"
+      (llvm, llvmHandle) <- mkstemps "output" ".ll"
+      let runtime = "../src/runtime.c"
+      -- write the llvmModule to a file
+      T.hPutStrLn llvmHandle (cs $ ppllvm llvmModule)
+      hClose llvmHandle
+      -- link the runtime with the assembly
+      callProcess
+        "clang"
+        ["-Wno-override-module", "-lm", llvm, runtime, "-o", "../" <> outfile]
