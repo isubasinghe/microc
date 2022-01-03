68e8224230379631aae380391aea291bdecb0a4d
isubasinghe <isubasinghe@student.unimelb.edu.au>
2022-01-03 21:20:29 +1100

    added compile function

---
+++ src/MicroUtils.hs
@@ -0,0 +1,9 @@
+module MicroUtils where 
+
+import Control.Monad.State
+locally :: MonadState s m => m a -> m a
+locally computation = do
+  oldState <- get
+  result   <- computation
+  put oldState
+  return result
